7dc0c3649e9951e3d96e6b0be2aac9d7cd439666
fliptu <fliptu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-26 13:54:54 +0000

    Importing 1.3
    git-svn-id: https://plugins.svn.wordpress.org/fliptu/tags/1.3@687512 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 1.3/fliptu-backup/trunk/fliptu_add.php
@@ -0,0 +1,50 @@
+<!-- Show the Edit  of flip ifram entries -->
+
+<?php  
+global $wpdb;
+
+?>
+
+
+
+		
+        	
+		<div id="page2">
+		<ul id="tabs">
+		<li id="tab1"><a href="admin.php?page=ic_add_embed_home">Home Settings</a></li>
+		<li id="tab2"><a href="admin.php?page=my-plugin-slug-items">Page Settings</a></li>
+		</ul></div> <br /><br />
+       		<strong>Create a new blog page and Embed a Fliptu flipstream or flipbook :</strong>
+       		 <br />
+        	<br />
+		<form action="" method="post" onsubmit="return validateAddForm()">
+		<input type='hidden' name='action' value="addData">	
+		<div id="ext_db_table_div">  
+		
+ 		<table>
+		
+		<tr>			
+			<td  style="width:102px;">Enter Page Title :</td> 
+			<td><input type="text" name="fliptu_title" id="fliptu_title" class="wdthtxt">
+			<span id="fliptu_titleError" style="display: none;color:#e35152;">
+			</td>
+		</tr>
+		<tr>
+			<td colspan="2">Copy the <b><?php echo htmlentities("<iframe>"); ?></b> code from the Export flipstream or Export flipbook wizard you generated from the
+
+desired paged on Fliptu.com and paste in the field below</td> 
+			
+		</tr>
+		<tr>
+			
+			<td colspan="2"><textarea name="fliptu_value" id="fliptu_value"  rows="5" class="wdth"></textarea>
+			<span id="fliptu_valueError" style="display: none;color:#e35152;">
+			</td>
+		</tr>	
+		
+		</table>
+		</div></br>
+		<input class="button-primary" type="submit" name="fliptuAdd"  value="Add Page">    
+		</form>	
+
+	
